c3bdffda861e14943ebaacc07242dd62c6126dfc
abolfazl <abolfazlrastegar1996@gmail.com>
2022-06-14 01:44:40 -0700

    Update Payments.php

--- src/Facades/Payments.php
+++ src/Facades/Payments.php
@@ -4,8 +4,5 @@ use Illuminate\Support\Facades\Facade;
 
 class Payments extends Facade
 {
-  protected static function getFacadeAccessor()
-  {
-      return 'payments';
-  }
+    protected static function getFacadeAccessor() { return 'payments'; }
 }
