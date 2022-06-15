1e75a1342adf9527d8d5dfc5f243087fd7db0203
abolfazl <abolfazlrastegar1996@gmail.com>
2022-06-15 03:57:05 -0700

    Update PaymentServiceProvider.php

--- src/Providers/PaymentServiceProvider.php
+++ src/Providers/PaymentServiceProvider.php
@@ -3,16 +3,16 @@
 namespace abolfazlrastegar\LaravelPayments\Providers;
 
 use Abolfazlrastegar\LaravelPayments\Payment;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class PaymentServiceProvider extends ServiceProvider
 {
     public function register ()
     {
-        $this->app->bind('payments', function () {
+      App::bind('payments', function () {
             return new Payment();
         });
-
     }
 
     public function boot ()
