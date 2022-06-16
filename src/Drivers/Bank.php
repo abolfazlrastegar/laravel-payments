479fa559b77b3b6754310db974c83c801b336703
abolfazl <abolfazlrastegar1996@gmail.com>
2022-06-16 05:08:49 -0700

    Update Bank.php

--- src/Drivers/Bank.php
+++ src/Drivers/Bank.php
@@ -4,6 +4,6 @@ namespace Abolfazlrastegar\LaravelPayments\Drivers;
 
 interface Bank
 {
-    public function request($api, $amount, $callbackURL);
+    public function request($api, $amount, $callbackURL, $info_user);
     public function verify($params);
 }
